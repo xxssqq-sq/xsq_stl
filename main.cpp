30828962afd824d4abf8fe9576a458921b1a2b0e
xxssqq-sq <54012979+xxssqq-sq@users.noreply.github.com>
2020-07-19 18:05:17 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+//        CTRL+N   查找类
+//        CTRL+SHIFT+N  查找文件
+//        CTRL+SHIFT+ALT+N 查 找类中的方法或变量
+//        CIRL+B   找变量的来源
+//        CTRL+ALT+B  找所有的子类
+//        CTRL+SHIFT+B  找变量的 类
+//        CTRL+G   定位行
+//        CTRL+F   在当前窗口查找文本
+//        CTRL+SHIFT+F  在指定窗口查找文本
+//        CTRL+R   在 当前窗口替换文本
+//        CTRL+SHIFT+R  在指定窗口替换文本
+//        ALT+SHIFT+C  查找修改的文件
+//        CTRL+E   最 近打开的文件
+//        F3   向下查找关键字出现位置
+//        SHIFT+F3  向上一个关键字出现位置
+//        F4   查找变量来源
+//        CTRL+ALT+F7  选 中的字符 查找工程出现的地方
+//        CTRL+SHIFT+O  弹出显示查找内容
